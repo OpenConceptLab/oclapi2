054742b36461ed444c7c95acc718a62800013c70
Sny <sny.aggarwal@gmail.com>
2020-06-24 10:27:11 +0530

    concept version names/descriptions list/get/put/post/delete urls

--- core/concepts/urls.py
+++ core/concepts/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
         name='concept-descriptions'
     ),
     re_path(
-        r'^(?P<concept>{pattern})/descriptions/(?P<id>{pattern})/$'.format(pattern=NAMESPACE_PATTERN),
+        r'^(?P<concept>{pattern})/descriptions/(?P<uuid>{pattern})/$'.format(pattern=NAMESPACE_PATTERN),
         views.ConceptDescriptionRetrieveUpdateDestroyView.as_view(),
         name='concept-description'
     ),
@@ -26,7 +26,7 @@ urlpatterns = [
         name='concept-names'
     ),
     re_path(
-        r'^(?P<concept>{pattern})/names/(?P<id>{pattern})/$'.format(pattern=NAMESPACE_PATTERN),
+        r'^(?P<concept>{pattern})/names/(?P<uuid>{pattern})/$'.format(pattern=NAMESPACE_PATTERN),
         views.ConceptNameRetrieveUpdateDestroyView.as_view(),
         name='concept-name'
     ),
@@ -50,5 +50,40 @@ urlpatterns = [
         views.ConceptVersionRetrieveView.as_view(),
         name='concept-version-detail'
     ),
-
+    re_path(
+        r'^(?P<concept>{pattern})/(?P<concept_version>{pattern})/descriptions/$'.format(pattern=NAMESPACE_PATTERN),
+        views.ConceptDescriptionListCreateView.as_view(),
+        name='concept-descriptions'
+    ),
+    re_path(
+        r'^(?P<concept>{pattern})/(?P<concept_version>{pattern})/descriptions/(?P<uuid>{pattern})/$'.format(
+            pattern=NAMESPACE_PATTERN
+        ),
+        views.ConceptDescriptionRetrieveUpdateDestroyView.as_view(),
+        name='concept-name'
+    ),
+    re_path(
+        r'^(?P<concept>{pattern})/(?P<concept_version>{pattern})/extras/$'.format(pattern=NAMESPACE_PATTERN),
+        views.ConceptExtrasView.as_view(),
+        name='concept-extras'
+    ),
+    re_path(
+        r'^(?P<concept>{pattern})/(?P<concept_version>{pattern})/extras/(?P<extra>{pattern})/$'.format(
+            pattern=NAMESPACE_PATTERN
+        ),
+        views.ConceptExtraRetrieveUpdateDestroyView.as_view(),
+        name='concept-extra'
+    ),
+    re_path(
+        r'^(?P<concept>{pattern})/(?P<concept_version>{pattern})/names/$'.format(pattern=NAMESPACE_PATTERN),
+        views.ConceptNameListCreateView.as_view(),
+        name='concept-names'
+    ),
+    re_path(
+        r'^(?P<concept>{pattern})/(?P<concept_version>{pattern})/names/(?P<uuid>{pattern})/$'.format(
+            pattern=NAMESPACE_PATTERN
+        ),
+        views.ConceptNameRetrieveUpdateDestroyView.as_view(),
+        name='concept-name'
+    ),
 ]
