3647b500d82a50325e771a0ab6cc170b8a2e63e6
Sny <sny.aggarwal@gmail.com>
2020-07-01 10:30:13 +0530

    sources urls ordering

--- core/sources/urls.py
+++ core/sources/urls.py
@@ -17,14 +17,14 @@ urlpatterns = [
     ),
     re_path(r"^(?P<source>{pattern})/concepts/".format(pattern=NAMESPACE_PATTERN), include('core.concepts.urls')),
     re_path(
-        r'^(?P<source>{pattern})/(?P<version>{pattern})/$'.format(pattern=NAMESPACE_PATTERN),
-        views.SourceVersionRetrieveUpdateDestroyView.as_view(),
-        name='sourceversion-detail'
-    ),
-    re_path(
         r"^(?P<source>{pattern})/extras/$".format(pattern=NAMESPACE_PATTERN),
         views.SourceExtrasView.as_view(),
         name='source-extras'
+    ),
+    re_path(
+        r'^(?P<source>{pattern})/(?P<version>{pattern})/$'.format(pattern=NAMESPACE_PATTERN),
+        views.SourceVersionRetrieveUpdateDestroyView.as_view(),
+        name='sourceversion-detail'
     ),
     re_path(
         r"^(?P<source>{pattern})/extras/(?P<extra>{pattern})/$".format(pattern=NAMESPACE_PATTERN),
