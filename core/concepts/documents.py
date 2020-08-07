d1a899e75453b62c8f35bc558cfe6fe38dfe6273
Sny <sny.aggarwal@gmail.com>
2020-08-07 09:32:54 +0530

    ES | concept document with indexes

--- core/concepts/documents.py
+++ core/concepts/documents.py
@@ -1,6 +1,7 @@
-from django_elasticsearch_dsl import Document
+from django_elasticsearch_dsl import Document, fields
 from django_elasticsearch_dsl.registries import registry
 
+from core.common.constants import HEAD
 from core.concepts.models import Concept
 
 
@@ -10,11 +11,49 @@ class ConceptDocument(Document):
         name = 'concepts'
         settings = {'number_of_shards': 1, 'number_of_replicas': 0}
 
+    id = fields.TextField(attr='mnemonic')
+    name = fields.TextField(attr='display_name')
+    lastUpdate = fields.DateField(attr='updated_at')
+    conceptClass = fields.TextField(attr='concept_class')
+    locale = fields.ListField(fields.TextField())
+    source = fields.TextField(attr='parent_resource')
+    owner = fields.TextField(attr='owner_name')
+    ownerType = fields.TextField(attr='owner_type')
+    source_version = fields.ListField(fields.IntegerField())
+    collection_version = fields.ListField(fields.IntegerField())
+    collection = fields.ListField(fields.IntegerField())
+    public_can_view = fields.BooleanField(attr='public_can_view')
+
     class Django:
         model = Concept
         fields = [
-            'mnemonic',
-            'concept_class',
             'datatype',
             'version',
+            'is_latest_version',
+            'retired',
+            'is_active'
         ]
+
+    @staticmethod
+    def prepare_locale(instance):
+        return list(
+            instance.names.filter(locale__isnull=False).distinct('locale').values_list('locale', flat=True)
+        )
+
+    @staticmethod
+    def prepare_source_version(instance):
+        return list(instance.sources.values_list('id', flat=True))
+
+    @staticmethod
+    def prepare_collection_version(instance):
+        return list(instance.collection_set.values_list('id', flat=True))
+
+    @staticmethod
+    def prepare_collection(instance):
+        from core.collections.models import Collection
+        return list(
+            Collection.objects.filter(
+                version=HEAD,
+                mnemonic__in=instance.collection_set.values_list('mnemonic', flat=True)
+            ).distinct('id').values_list('id', flat=True)
+        )
