fd7d75bdff9a8515f10f9344d76bae83d3351535
Sny <sny.aggarwal@gmail.com>
2020-08-25 22:49:50 +0530

    bulk-importer | added custom query parameters for swagger

--- core/importers/views.py
+++ core/importers/views.py
@@ -1,6 +1,8 @@
 import uuid
 
 from celery.result import AsyncResult
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
@@ -8,28 +10,41 @@ from rest_framework.views import APIView
 
 from core.common.tasks import bulk_import, bulk_priority_import
 
+task_param = openapi.Parameter(
+    'task', openapi.IN_QUERY, description="task uuid (mandatory)", type=openapi.TYPE_STRING
+)
+result_param = openapi.Parameter(
+    'result', openapi.IN_QUERY, description="result format (json | report) (optional)", type=openapi.TYPE_STRING
+)
+update_if_exists_param = openapi.Parameter(
+    'update_if_exists', openapi.IN_QUERY, description="true | false (mandatory)", type=openapi.TYPE_STRING,
+    default='true'
+)
+
 
 class BulkImportView(APIView):
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated, )
 
+    @swagger_auto_schema(manual_parameters=[update_if_exists_param])
     def post(self, request):
         username = self.request.user.username
         update_if_exists = request.GET.get('update_if_exists', 'true')
         if update_if_exists not in ['true', 'false']:
             return Response(
-                {'exception': 'update_if_exists must be either \'true\' or \'false\''},
+                dict(exception="update_if_exists must be either 'true' or 'false'"),
                 status=status.HTTP_400_BAD_REQUEST
             )
         update_if_exists = update_if_exists == 'true'
 
         task_id = str(uuid.uuid4()) + '-' + username
-        if username == 'root':
+        if username in ['root', 'ocladmin']:
             task = bulk_priority_import.apply_async((request.body, username, update_if_exists), task_id=task_id)
         else:
             task = bulk_import.apply_async((request.body, username, update_if_exists), task_id=task_id)
 
         return Response(dict(task=task.id, state=task.state))
 
+    @swagger_auto_schema(manual_parameters=[task_param, result_param])
     def get(self, request):  # pylint: disable=too-many-return-statements
         task_id = request.GET.get('task')
         result_format = request.GET.get('result')
@@ -54,6 +69,6 @@ class BulkImportView(APIView):
             return Response(result.detailed_summary)
 
         if task.failed():
-            return Response({'exception': str(task.result)}, status=status.HTTP_400_BAD_REQUEST)
+            return Response(dict(exception=str(task.result)), status=status.HTTP_400_BAD_REQUEST)
 
         return Response(dict(task=task.id, state=task.state))
