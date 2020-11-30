0cd5c37f02f3064ca2903071fabb032a332547b0
Sny <sny.aggarwal@gmail.com>
2020-11-30 14:24:00 +0530

    pylint | unused import

--- core/orgs/views.py
+++ core/orgs/views.py
@@ -3,7 +3,7 @@ from django.http import Http404
 from pydash import get
 from rest_framework import mixins, status, generics
 from rest_framework.generics import RetrieveAPIView, DestroyAPIView, RetrieveUpdateDestroyAPIView
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
