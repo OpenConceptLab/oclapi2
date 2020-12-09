6e498bfa9322ade0ecba1e94ea26167aa6b6455f
Rafal Korytkowski <rafal.korytkowski@gmail.com>
2020-12-09 11:37:35 +0100

    Fixing to use FLOWER_HOST and FLOWER_PORT

--- core/settings.py
+++ core/settings.py
@@ -304,5 +304,7 @@ ENV = os.environ.get('ENVIRONMENT', 'development')
 # Only used for flower
 FLOWER_USER = os.environ.get('FLOWER_USER', 'root')
 FLOWER_PWD = os.environ.get('FLOWER_PWD', 'Root123')
+FLOWER_HOST = os.environ.get('FLOWER_HOST', 'flower')
+FLOWER_PORT = os.environ.get('FLOWER_PORT', 5555)
 DATA_UPLOAD_MAX_MEMORY_SIZE = 100*1024*1024
 FILE_UPLOAD_MAX_MEMORY_SIZE = 100*1024*1024
