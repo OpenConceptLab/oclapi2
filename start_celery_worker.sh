432c854cbe0f8c84dddc2546a6a79929c5497214
Rafal Korytkowski <rafal.korytkowski@gmail.com>
2021-01-14 16:12:40 +0100

    Using unique celery node names (AWS deployment)

--- start_celery_worker.sh
+++ start_celery_worker.sh
@@ -4,4 +4,7 @@ set -e
 
 ./wait_for_it.sh ${REDIS_HOST}:${REDIS_PORT} -t 0
 
-celery worker -A core.celery -l INFO "$@"
+CELERY_WORKER_NAME=${CELERY_WORKER_NAME:-""}
+UUID=$(cat /proc/sys/kernel/random/uuid)
+
+celery worker -A core.celery -n "${CELERY_WORKER_NAME}-${UUID}" -l INFO "$@"
