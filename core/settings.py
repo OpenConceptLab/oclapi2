d44b442e9bc0ca44983065e0375aedd52c9d5ca0
Sny <sny.aggarwal@gmail.com>
2021-01-29 12:54:46 +0530

    Added gzip middleware | compresses all outgoing calls

--- core/settings.py
+++ core/settings.py
@@ -115,6 +115,7 @@ REDOC_SETTINGS = {
 }
 
 MIDDLEWARE = [
+    'django.middleware.gzip.GZipMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
