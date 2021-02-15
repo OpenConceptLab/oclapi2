9115b1e82d47037b5b91314de0975e5b73137e9c
Sny <sny.aggarwal@gmail.com>
2021-02-15 18:58:13 +0530

    excluding healthcheck routes from swagger

--- core/common/healthcheck/views.py
+++ core/common/healthcheck/views.py
@@ -8,23 +8,27 @@ from core.common.healthcheck.healthcheck import FlowerHealthCheck, CeleryDefault
     ESHealthCheck
 
 
-class FlowerHealthcheckView(MainView):
+class BaseHealthcheckView(MainView):
+    swagger_schema = None
+
+
+class FlowerHealthcheckView(BaseHealthcheckView):
     _plugins = [FlowerHealthCheck(critical_service=True)]
 
 
-class ESHealthcheckView(MainView):
+class ESHealthcheckView(BaseHealthcheckView):
     _plugins = [ESHealthCheck(critical_service=True)]
 
 
-class RedisHealthcheckView(MainView):
+class RedisHealthcheckView(BaseHealthcheckView):
     _plugins = [RedisHealthCheck()]
 
 
-class DBHealthcheckView(MainView):
+class DBHealthcheckView(BaseHealthcheckView):
     _plugins = [DatabaseBackend()]
 
 
-class CeleryHealthCheckView(MainView):
+class CeleryHealthCheckView(BaseHealthcheckView):
     _plugins = [
         CeleryDefaultQueueHealthCheck(critical_service=True),
         CeleryConcurrentThreadsHealthCheck(),
@@ -36,29 +40,29 @@ class CeleryHealthCheckView(MainView):
     ]
 
 
-class CeleryDefaultHealthCheckView(MainView):
+class CeleryDefaultHealthCheckView(BaseHealthcheckView):
     _plugins = [CeleryDefaultQueueHealthCheck(critical_service=True)]
 
 
-class CeleryBulkImport0HealthCheckView(MainView):
+class CeleryBulkImport0HealthCheckView(BaseHealthcheckView):
     _plugins = [CeleryBulkImport0QueueHealthCheck(critical_service=True)]
 
 
-class CeleryBulkImport1HealthCheckView(MainView):
+class CeleryBulkImport1HealthCheckView(BaseHealthcheckView):
     _plugins = [CeleryBulkImport1QueueHealthCheck(critical_service=True)]
 
 
-class CeleryBulkImport2HealthCheckView(MainView):
+class CeleryBulkImport2HealthCheckView(BaseHealthcheckView):
     _plugins = [CeleryBulkImport2QueueHealthCheck(critical_service=True)]
 
 
-class CeleryBulkImport3HealthCheckView(MainView):
+class CeleryBulkImport3HealthCheckView(BaseHealthcheckView):
     _plugins = [CeleryBulkImport3QueueHealthCheck(critical_service=True)]
 
 
-class CeleryBulkImportRootHealthCheckView(MainView):
+class CeleryBulkImportRootHealthCheckView(BaseHealthcheckView):
     _plugins = [CeleryBulkImportRootQueueHealthCheck(critical_service=True)]
 
 
-class CeleryConcurrentThreadsHealthCheckView(MainView):
+class CeleryConcurrentThreadsHealthCheckView(BaseHealthcheckView):
     _plugins = [CeleryConcurrentThreadsHealthCheck(critical_service=True)]
