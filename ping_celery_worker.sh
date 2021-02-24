d4247dc707566e880459eb6bf0529977ea12c019
Rafal Korytkowski <rafal.korytkowski@gmail.com>
2021-02-24 11:42:54 +0100

    OpenConceptLab/ocl_issues#559 Optimizing celery healthchecks

--- ping_celery_worker.sh
+++ ping_celery_worker.sh
@@ -5,4 +5,5 @@ set -e
 CELERY_WORKER_NAME=${CELERY_WORKER_NAME:-""}
 CELERY_WORKER_NAME_WITH_UUID=`cat celery-worker-$CELERY_WORKER_NAME.tmp`
 
-celery inspect ping -A core.celery -d "celery@$CELERY_WORKER_NAME_WITH_UUID"
+# Providing broker with -b is more efficient than -A as the app does not have to be initialized on each ping
+celery inspect ping -d "celery@$CELERY_WORKER_NAME_WITH_UUID" -b redis://${REDIS_HOST:-localhost}:${REDIS_PORT:-6379}/0
