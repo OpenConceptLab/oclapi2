bd4c7b75929d247c4f9039b0890c4b982aae1ed6
Sny <sny.aggarwal@gmail.com>
2021-03-26 15:53:39 +0530

    user serializers | company/location/website are optional

--- core/users/serializers.py
+++ core/users/serializers.py
@@ -28,14 +28,14 @@ class UserCreateSerializer(serializers.ModelSerializer):
         UniqueValidator(queryset=UserProfile.objects.all(), message='A user with this username already exists')
     ])
     name = serializers.CharField(required=False)
-    first_name = serializers.CharField(required=False, write_only=True)
-    last_name = serializers.CharField(required=False, write_only=True)
+    first_name = serializers.CharField(required=False, write_only=True, allow_blank=True)
+    last_name = serializers.CharField(required=False, write_only=True, allow_blank=True)
     email = serializers.CharField(required=True, validators=[
         UniqueValidator(queryset=UserProfile.objects.all(), message='A user with this email already exists')
     ])
     password = serializers.CharField(required=False, write_only=True)
-    company = serializers.CharField(required=False)
-    location = serializers.CharField(required=False)
+    company = serializers.CharField(required=False, allow_blank=True, allow_null=True)
+    location = serializers.CharField(required=False, allow_blank=True, allow_null=True)
     preferred_locale = serializers.CharField(required=False)
     orgs = serializers.IntegerField(read_only=True, source='orgs_count')
     created_on = serializers.DateTimeField(source='created_at', read_only=True)
@@ -45,13 +45,14 @@ class UserCreateSerializer(serializers.ModelSerializer):
     extras = serializers.JSONField(required=False, allow_null=True)
     token = serializers.CharField(required=False, read_only=True)
     verified = serializers.BooleanField(required=False, default=True)
+    website = serializers.CharField(required=False, allow_blank=True, allow_null=True)
 
     class Meta:
         model = UserProfile
         fields = (
             'type', 'uuid', 'username', 'name', 'email', 'company', 'location', 'preferred_locale', 'orgs',
             'public_collections', 'public_sources', 'created_on', 'updated_on', 'created_by', 'updated_by',
-            'url', 'extras', 'password', 'token', 'verified', 'first_name', 'last_name'
+            'url', 'extras', 'password', 'token', 'verified', 'first_name', 'last_name', 'website'
         )
 
     def create(self, validated_data):
@@ -101,13 +102,13 @@ class UserDetailSerializer(serializers.ModelSerializer):
     type = serializers.CharField(source='resource_type', read_only=True)
     uuid = serializers.CharField(source='id', read_only=True)
     username = serializers.CharField(required=False)
-    first_name = serializers.CharField(required=False)
-    last_name = serializers.CharField(required=False)
+    first_name = serializers.CharField(required=False, allow_blank=True)
+    last_name = serializers.CharField(required=False, allow_blank=True)
     name = serializers.CharField(required=False)
     email = serializers.CharField(required=False)
-    company = serializers.CharField(required=False)
-    website = serializers.CharField(required=False)
-    location = serializers.CharField(required=False)
+    company = serializers.CharField(required=False, allow_blank=True, allow_null=True)
+    website = serializers.CharField(required=False, allow_blank=True, allow_null=True)
+    location = serializers.CharField(required=False, allow_blank=True, allow_null=True)
     preferred_locale = serializers.CharField(required=False)
     orgs = serializers.IntegerField(read_only=True, source='orgs_count')
     created_on = serializers.DateTimeField(source='created_at', read_only=True)
