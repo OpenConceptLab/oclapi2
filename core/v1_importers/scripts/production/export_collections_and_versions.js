ed0895295f8277f94e24f56f7e50d688d3e69033
Sny <sny.aggarwal@gmail.com>
2021-04-09 22:45:37 +0530

    OpenConceptLab/ocl_issues#412 | v1 production exports | added covid start kit collection

--- core/v1_importers/scripts/production/export_collections_and_versions.js
+++ core/v1_importers/scripts/production/export_collections_and_versions.js
@@ -5,8 +5,7 @@ db.export.collectionversion_ids.drop()
 
 
 org_ids = db.orgs_organization.find({mnemonic: {$in: ["OHRITechGroup", "OMRSCOVIDSquad", "EthiopiaNHDD", "MSF-OCB", "MOH-DM", "IAD", "integrated-impact", "SSAS", "DSME-Test", "GFPVAN", "im", "Kuunika", "DSME", "DSME-CDD", "MOH", "mTOMADY", "IRDO", "ibwighane", "mw-terminology-service", "mw-product-master", "ICI", "mw-terminology-service-development", "mw-product-master-ocl-instance", "mw-product-master-ocl", "malawi-diseases-diagnosis", "TestOrg", "DWB", "CMDF", "MUDHC", "MSF", "MU", "MUDH", "nproto", "MSFTW", "TWABC", "kuunika-registries", "UNIMED", "SHC", "MSFOCP", "SELF", "OpenSandbox", "sandbox", "ATH", "Reverton"]}}, {_id: 1}).map(doc => doc._id.str);
-collection_ids = db.collection_collection.find({parent_id: {$in: org_ids}}, {_id: 1}).map(doc => doc._id.str);
-
+collection_ids = db.collection_collection.find({ $or: [{parent_id: {$in: org_ids}}, {uri: '/orgs/CIEL/collections/COVID-19-Starter-Set/'}]}, {_id: 1}).map(doc => doc._id.str);
 db.export.collections.insertMany(db.collection_collection.find({parent_id: {$in: org_ids}}).map(doc => doc));
 db.export.collectionversions.insertMany(db.collection_collectionversion.find({versioned_object_id: {$in: collection_ids}, mnemonic: {$ne: 'HEAD'}}).map(doc => doc));
 db.export.collection_ids.insertMany(db.collection_collection.find({}).map(doc => ({_id: doc._id, uri: doc.uri})))
