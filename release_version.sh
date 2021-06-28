d6344b3257b3cb5f8412f7538b3cf0e114d9ad62
Rafal Korytkowski <rafal.korytkowski@gmail.com>
2021-06-28 13:38:40 +0200

    Increase maintenance version after release

--- release_version.sh
+++ release_version.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 set -e
 
+VERSION_FILE="core/__init__.py"
+
 SOURCE_COMMIT=$(git rev-parse HEAD)
 export SOURCE_COMMIT=${SOURCE_COMMIT:0:8}
 
-PACKAGE_VERSION=$(cat core/__init__.py \
+PROJECT_VERSION=$(cat $VERSION_FILE \
   | grep API_VERSION \
   | head -1 \
   | awk -F= '{ print $2 }' \
   | sed "s/[',]//g" \
   | tr -d '[[:space:]]')
 
-TAG="$PACKAGE_VERSION-$SOURCE_COMMIT"
+TAG="$PROJECT_VERSION-$SOURCE_COMMIT"
 
 ./set_build_version.sh
 
-git add core/__init__.py
-git commit -m "Set build version"
+git checkout -b release
+git add $VERSION_FILE
+git commit -m "Release version $PROJECT_VERSION"
 
 git tag "$TAG"
 
@@ -27,3 +30,14 @@ docker pull $DOCKER_IMAGE_ID
 docker tag $DOCKER_IMAGE_ID $DOCKER_IMAGE_NAME:$TAG
 docker push $DOCKER_IMAGE_NAME:$TAG
 
+if [[ "$INCREASE_MAINTENANCE_VERSION" = true ]]; then
+  git checkout master
+
+  NEW_PROJECT_VERSION=$(echo "${PROJECT_VERSION}" | awk -F. -v OFS=. '{$NF++;print}')
+  sed -i "s/API_VERSION = \"$PROJECT_VERSION\"/API_VERSION = \"$NEW_PROJECT_VERSION\"/" $VERSION_FILE
+
+  git add $VERSION_FILE
+  git commit -m "Increase maintenance version to $NEW_PROJECT_VERSION"
+
+  git push origin master
+fi
