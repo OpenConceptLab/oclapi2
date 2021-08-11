73417b4ebe84e03a45a9a93ae0a9c90b467f9dd2
Rafal Korytkowski <rafal.korytkowski@gmail.com>
2021-08-11 13:00:54 +0200

    OpenConceptLab/ocl_issues#845 Adding missing composite index

--- core/mappings/models.py
+++ core/mappings/models.py
@@ -20,7 +20,9 @@ class Mapping(MappingValidationMixin, SourceChildMixin, VersionedModel):
     class Meta:
         db_table = 'mappings'
         unique_together = ('mnemonic', 'version', 'parent')
-        indexes = [] + VersionedModel.Meta.indexes
+        indexes = [
+                      models.Index(fields=['is_active', 'retired', 'is_latest_version', 'public_access']),
+                  ] + VersionedModel.Meta.indexes
 
     parent = models.ForeignKey('sources.Source', related_name='mappings_set', on_delete=models.CASCADE)
     map_type = models.TextField(db_index=True)
