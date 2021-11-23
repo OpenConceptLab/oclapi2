b080fe96cc2316034e769e42e1cf297ae2086a0b
Sny <sny.aggarwal@gmail.com>
2021-11-23 08:34:05 +0530

    coverage script

--- coverage.sh
+++ coverage.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 LOG=0 coverage run --parallel-mode --source='core' manage.py test --parallel=4 -v 3 --keepdb
 coverage combine
-coverage report -m --include=core/* --fail-under=100
+coverage report -m --include=core/* --fail-under=80
