faeb50634f39e51e1c554569f3da602239dd058d
Sny <sny.aggarwal@gmail.com>
2021-12-29 13:00:54 +0530

    coverage.sh | fail if tests fail

--- coverage.sh
+++ coverage.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+set -e
 COVERAGE_FILE=/tmp/.coverage coverage run --parallel-mode --source='core' manage.py test --parallel=4 -v 3 --keepdb
 COVERAGE_FILE=/tmp/.coverage coverage combine
 COVERAGE_FILE=/tmp/.coverage coverage report -m --include=core/* --fail-under=85 --sort=cover
