f28d469bc7e4cd253810b598136fbf87840b3057
Sny <sny.aggarwal@gmail.com>
2022-01-18 13:22:59 +0530

    OpenConceptLab/ocl_issues#712 | utils | method to get values from nested dict

--- core/common/utils.py
+++ core/common/utils.py
@@ -711,3 +711,11 @@ def named_tuple_fetchall(cursor):
     desc = cursor.description
     nt_result = namedtuple('Result', [col[0] for col in desc])
     return [nt_result(*row) for row in cursor.fetchall()]
+
+
+def nested_dict_values(d):
+    for value in d.values():
+        if isinstance(value, dict):
+            yield from nested_dict_values(value)
+        else:
+            yield value
