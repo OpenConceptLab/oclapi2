3982488c3a9562dbc59c71e9f37e5b46ad617b0f
Rafal Korytkowski <rafal.korytkowski@gmail.com>
2022-03-25 12:38:55 +0100

    OpenConceptLab/ocl_issues#1244 Using 2 instead of 4 parallel workers for tests

--- coverage.sh
+++ coverage.sh
@@ -3,6 +3,6 @@ set -e
 
 ./wait_for_it.sh ${ES_HOST}:${ES_PORT} -t 0
 
-COVERAGE_FILE=/tmp/.coverage coverage run --parallel-mode --source='core' manage.py test --parallel=4 -v 3 --keepdb
+COVERAGE_FILE=/tmp/.coverage coverage run --parallel-mode --source='core' manage.py test --parallel=2 -v 3 --keepdb
 COVERAGE_FILE=/tmp/.coverage coverage combine
 COVERAGE_FILE=/tmp/.coverage coverage report -m --include=core/* --fail-under=88 --sort=cover
