c3bc1b526885ae68eb1cfbdb3d34feeface8fcee
Rafal Korytkowski <rafal.korytkowski@gmail.com>
2022-03-25 12:22:25 +0100

    OpenConceptLab/ocl_issues#1244 | Wait for ES when running tests

--- coverage.sh
+++ coverage.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 set -e
+
+./wait_for_it.sh ${ES_HOST}:${ES_PORT} -t 0
+
 COVERAGE_FILE=/tmp/.coverage coverage run --parallel-mode --source='core' manage.py test --parallel=4 -v 3 --keepdb
 COVERAGE_FILE=/tmp/.coverage coverage combine
 COVERAGE_FILE=/tmp/.coverage coverage report -m --include=core/* --fail-under=88 --sort=cover
