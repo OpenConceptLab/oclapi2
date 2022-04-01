a6e21ec810332ef34de93228b721f41b7347ffc3
Sny <sny.aggarwal@gmail.com>
2022-04-01 13:52:20 +0530

    coverage at 90

--- coverage.sh
+++ coverage.sh
@@ -5,4 +5,4 @@ set -e
 
 COVERAGE_FILE=/tmp/.coverage coverage run --parallel-mode --source='core' manage.py test --parallel=1 -v 3 --keepdb
 COVERAGE_FILE=/tmp/.coverage coverage combine
-COVERAGE_FILE=/tmp/.coverage coverage report -m --include=core/* --fail-under=88 --sort=cover
+COVERAGE_FILE=/tmp/.coverage coverage report -m --include=core/* --fail-under=90 --sort=cover
