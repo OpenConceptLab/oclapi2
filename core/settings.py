02b6042090a5afbf299ec5f6b96bd00a82a2f16d
Sny <sny.aggarwal@gmail.com>
2022-04-07 11:50:47 +0530

    OpenConceptLab/ocl_issues#1262 | (attempting) fixing whitenoise static files issue

--- core/settings.py
+++ core/settings.py
@@ -269,7 +269,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
 STATIC_ROOT = '/staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 AUTH_USER_MODEL = 'users.UserProfile'
 TEST_RUNNER = 'core.common.tests.CustomTestRunner'
