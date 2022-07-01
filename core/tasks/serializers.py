5bcb6fb628f82d30441bf0f85cbc22bf25d8a53f
Sny <sny.aggarwal@gmail.com>
2022-07-01 10:26:46 +0530

    Fixing typo

--- core/tasks/serializers.py
+++ core/tasks/serializers.py
@@ -4,5 +4,5 @@ from rest_framework.serializers import Serializer
 
 class FlowerTaskSerializer(Serializer):  # pylint: disable=abstract-method
     task_id = CharField(read_only=True, source='task-id')
-    state = CharField(read_only=True, source='state')
+    state = CharField(read_only=True)
     result = JSONField(allow_null=True, read_only=True)
