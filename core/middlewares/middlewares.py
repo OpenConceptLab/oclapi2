2aff93f76f0812edca0bacf03f9388afb5ed8894
Sny <sny.aggarwal@gmail.com>
2022-07-13 13:59:42 +0530

    Logs | added request method in response headers

--- core/middlewares/middlewares.py
+++ core/middlewares/middlewares.py
@@ -2,7 +2,8 @@ import logging
 import time
 
 from request_logging.middleware import LoggingMiddleware
-from core.common.constants import VERSION_HEADER, REQUEST_USER_HEADER, RESPONSE_TIME_HEADER, REQUEST_URL_HEADER
+from core.common.constants import VERSION_HEADER, REQUEST_USER_HEADER, RESPONSE_TIME_HEADER, REQUEST_URL_HEADER, \
+    REQUEST_METHOD_HEADER
 from core.common.utils import set_current_user, set_request_url
 
 request_logger = logging.getLogger('request_logger')
@@ -47,6 +48,7 @@ class ResponseHeadersMiddleware(BaseMiddleware):
         response[REQUEST_USER_HEADER] = str(getattr(request, 'user', None))
         response[RESPONSE_TIME_HEADER] = time.time() - start_time
         response[REQUEST_URL_HEADER] = request.path
+        response[REQUEST_METHOD_HEADER] = request.method
         return response
 
 
