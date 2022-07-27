59c476e6f7272f31cf7db15b441c760a5f8d4344
Sny <sny.aggarwal@gmail.com>
2022-07-27 08:49:27 +0530

    Extracting env vars for email setting

--- core/settings.py
+++ core/settings.py
@@ -363,9 +363,9 @@ EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', True) in ['true', True]
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'no-reply@openconceptlab.org')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
 EMAIL_PORT = os.environ.get('EMAIL_PORT', 587)
-COMMUNITY_EMAIL = 'community@openconceptlab.org'
-DEFAULT_FROM_EMAIL = 'openconceptlab <noreply@openconceptlab.org>'
-ACCOUNT_EMAIL_SUBJECT_PREFIX = '[openconceptlab.org] '
+COMMUNITY_EMAIL = os.environ.get('COMMUNITY_EMAIL', 'community@openconceptlab.org')
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', 'openconceptlab <noreply@openconceptlab.org>')
+ACCOUNT_EMAIL_SUBJECT_PREFIX = os.environ.get('ACCOUNT_EMAIL_SUBJECT_PREFIX', '[openconceptlab.org] ')
 ADMINS = (
     ('Jonathan Payne', 'paynejd@gmail.com'),
 )
