5a54684dc105e9fd247c7a6c1f4e061ce2ce77d2
Rafal Korytkowski <rafal.korytkowski@gmail.com>
2022-12-13 11:05:50 +0100

    OpenConceptLab/ocl_issues#1430 Adjusting logging

--- startup.sh
+++ startup.sh
@@ -6,8 +6,8 @@ set -e
 ./wait_for_it.sh ${ES_HOST}:${ES_PORT} -t 0
 ./wait_for_it.sh ${REDIS_HOST}:${REDIS_PORT} -t 0
 
-echo "Running DB migrations"
 if [[ "$SKIP_MIGRATE_DB" != "true" ]]; then
+  echo "Running DB migrations"
   python manage.py migrate
 fi
 
