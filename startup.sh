ffeb1227ba8a8617dfbb38eb0e6f60a2ded5c2c4
Rafal Korytkowski <rafal.korytkowski@gmail.com>
2022-12-13 11:04:54 +0100

    OpenConceptLab/ocl_issues#1430 Support DB migrations in background

--- startup.sh
+++ startup.sh
@@ -7,7 +7,9 @@ set -e
 ./wait_for_it.sh ${REDIS_HOST}:${REDIS_PORT} -t 0
 
 echo "Running DB migrations"
-python manage.py migrate
+if [[ "$SKIP_MIGRATE_DB" != "true" ]]; then
+  python manage.py migrate
+fi
 
 echo "Importing base entities"
 python manage.py loaddata core/fixtures/*
