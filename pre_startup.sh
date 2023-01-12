285a638b5ad71c7dfebf720057d486dc20ea4f42
Sny <sny.aggarwal@gmail.com>
2023-01-12 09:39:27 +0530

    OpenConceptLab/ocl_issues#1430 | separating pre_startup script | includes migrate and other tasks

---
+++ pre_startup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+./wait_for_it.sh ${DB_HOST}:${DB_PORT} -t 0
+./wait_for_it.sh ${ES_HOST}:${ES_PORT} -t 0
+./wait_for_it.sh ${REDIS_HOST}:${REDIS_PORT} -t 0
+echo "Running DB migrations"
+python manage.py migrate
+echo "Importing base entities"
+python manage.py loaddata core/fixtures/*
+
+echo "Setting up superuser"
+python manage.py setup_superuser
+
+echo "Importing lookup values"
+python manage.py import_lookup_values
+
+echo "Populating text from extras.about"
+python manage.py populate_text_from_extras_about
--- startup.sh
+++ startup.sh
@@ -7,19 +7,7 @@ set -e
 ./wait_for_it.sh ${REDIS_HOST}:${REDIS_PORT} -t 0
 
 if [[ "$SKIP_MIGRATE_DB" != "true" ]]; then
-  echo "Running DB migrations"
-  python manage.py migrate
-  echo "Importing base entities"
-  python manage.py loaddata core/fixtures/*
-
-  echo "Setting up superuser"
-  python manage.py setup_superuser
-
-  echo "Importing lookup values"
-  python manage.py import_lookup_values
-
-  echo "Populating text from extras.about"
-  python manage.py populate_text_from_extras_about
+    /bin/bash ./pre_startup.sh
 fi
 
 
