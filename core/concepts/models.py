a351f0cb1dc4bd1e3584236a57d2beb0ec7b7ce5
Sny <sny.aggarwal@gmail.com>
2023-03-03 09:06:12 +0530

    OpenConceptLab/ocl_issues#1467 | added index on concept_class and datatype

--- core/concepts/models.py
+++ core/concepts/models.py
@@ -231,8 +231,8 @@ class Concept(ConceptValidationMixin, SourceChildMixin, VersionedModel):  # pyli
         ] + VersionedModel.Meta.indexes
 
     external_id = models.TextField(null=True, blank=True)
-    concept_class = models.TextField()
-    datatype = models.TextField()
+    concept_class = models.TextField(db_index=True)
+    datatype = models.TextField(db_index=True)
     comment = models.TextField(null=True, blank=True)
     parent = models.ForeignKey('sources.Source', related_name='concepts_set', on_delete=models.CASCADE)
     sources = models.ManyToManyField('sources.Source', related_name='concepts')
