1fe9ad4625914d4398bbf3e9f6df9ec5aaa4d48a
Sny <sny.aggarwal@gmail.com>
2023-08-21 12:33:18 +0530

    OpenConceptLab/ocl_issues#1635 | fixing tests for default version HEAD when not global search

--- core/integration_tests/tests_concepts.py
+++ core/integration_tests/tests_concepts.py
@@ -1695,12 +1695,11 @@ class ConceptListViewTest(OCLAPITestCase):
         self.assertTrue(response.data[0]['total'] >= 1)
 
         response = self.client.get(
-            self.source.concepts_url + '?q=MyConcept',
+            self.source.concepts_url + '?q=MyConcept',  # assumes HEAD
             HTTP_AUTHORIZATION='Token ' + self.token,
         )
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(response.data), 1)
-        self.assertEqual(response.data[0]['id'], 'MyConcept2')
+        self.assertEqual(len(response.data), 2)
 
         response = self.client.get(
             self.source.uri + 'v1/concepts/?q=MyConcept',
@@ -1718,14 +1717,6 @@ class ConceptListViewTest(OCLAPITestCase):
         self.assertEqual(len(response.data), 2)
         self.assertEqual(response.data[0]['id'], 'MyConcept1')
         self.assertEqual(response.data[1]['id'], 'MyConcept2')
-
-        response = self.client.get(
-            self.source.concepts_url + '?q=MyConcept',
-            HTTP_AUTHORIZATION='Token ' + self.token,
-        )
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(response.data), 1)
-        self.assertEqual(response.data[0]['id'], 'MyConcept2')
 
     def test_facets(self):
         if settings.ENV == 'ci':
@@ -1761,16 +1752,19 @@ class ConceptListViewTest(OCLAPITestCase):
         self.assertFalse(class_b_facet[2])
 
         response = self.client.get(
-            self.source.concepts_url + '?facetsOnly=true'
+            self.source.concepts_url + '?facetsOnly=true'  # assumes HEAD
         )
         self.assertEqual(response.status_code, 200)
         self.assertEqual(list(response.data.keys()), ['facets'])
 
         class_b_facet = [x for x in response.data['facets']['fields']['conceptClass'] if x[0] == 'classb'][0]
+        class_a_facet = [x for x in response.data['facets']['fields']['conceptClass'] if x[0] == 'classa'][0]
         self.assertEqual(class_b_facet[0], 'classb')
         self.assertTrue(class_b_facet[1] >= 1)
         self.assertFalse(class_b_facet[2])
-        self.assertEqual([x for x in response.data['facets']['fields']['conceptClass'] if x[0] == 'classa'], [])
+        self.assertEqual(class_a_facet[0], 'classa')
+        self.assertTrue(class_a_facet[1] >= 1)
+        self.assertFalse(class_a_facet[2])
 
         response = self.client.get(
             self.source.uri + 'v1/concepts/?facetsOnly=true'
