9ee0e0512f1460ff44396cc3bfa347dfb59dfd48
rkorytkowski <rafal.korytkowski@gmail.com>
2023-08-23 12:06:36 +0200

    OpenConceptLab/ocl_issues#927 Redis clustering, fixing celery startup

--- start_celery_worker.sh
+++ start_celery_worker.sh
@@ -10,4 +10,4 @@ CELERY_WORKER_NAME_WITH_UUID="${CELERY_WORKER_NAME}-${UUID}"
 
 echo "$CELERY_WORKER_NAME_WITH_UUID" > "/temp/celery-worker-$CELERY_WORKER_NAME.tmp"
 
-celery -A core.celery worker -n $CELERY_WORKER_NAME_WITH_UUID --loglevel=INFO "$@" -E --without-heartbeat --without-gossip --without-mingle
+celery -A core worker -n $CELERY_WORKER_NAME_WITH_UUID --loglevel=INFO "$@" -E --without-heartbeat --without-gossip --without-mingle
--- start_flower.sh
+++ start_flower.sh
@@ -4,4 +4,4 @@ set -e
 
 ./wait_for_it.sh ${REDIS_HOST}:${REDIS_PORT} -t 0
 
-celery -A core.celery flower --basic_auth=${FLOWER_USER}:${FLOWER_PASSWORD} --conf=flowerconfig.py
+celery -A core flower --basic_auth=${FLOWER_USER}:${FLOWER_PASSWORD} --conf=flowerconfig.py
