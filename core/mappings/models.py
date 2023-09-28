b72d95570a444806eca425bb7aadac5326d4c857
Sny <sny.aggarwal@gmail.com>
2023-09-28 11:26:31 +0530

    OpenConceptLab/ocl_issues#1659 | Mapping initial version to have the right updated_by:

--- core/mappings/models.py
+++ core/mappings/models.py
@@ -284,6 +284,7 @@ class Mapping(MappingValidationMixin, SourceChildMixin, VersionedModel):
     @classmethod
     def create_initial_version(cls, mapping, **kwargs):
         initial_version = mapping.clone()
+        initial_version.created_by = initial_version.updated_by = mapping.created_by
         initial_version.comment = mapping.comment
         initial_version.save(**kwargs)
         initial_version.version = initial_version.id
