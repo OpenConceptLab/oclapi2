9dbdfc9c5549649ae4884a4d691d391d1a7ac9d0
Sny <sny.aggarwal@gmail.com>
2023-10-04 18:52:43 +0530

    pylint | remove unused import

--- core/collections/views.py
+++ core/collections/views.py
@@ -2,7 +2,6 @@ import logging
 
 from celery_once import AlreadyQueued
 from django.conf import settings
-from django.core.exceptions import ValidationError
 from django.db import IntegrityError
 from django.db.models import Q
 from django.http import Http404
