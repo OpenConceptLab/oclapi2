0c187a9f0279a7fb468db7648e76d2e32ab789d8
Sny <sny.aggarwal@gmail.com>
2024-03-27 19:18:07 +0530

    OpenConceptLab/ocl_issues#1777 | removed unused flower task serializer

--- core/tasks/serializers.py
+++ core/tasks/serializers.py
@@ -1,13 +1,7 @@
-from rest_framework.fields import CharField, JSONField
-from rest_framework.serializers import Serializer, ModelSerializer
+from rest_framework.fields import CharField
+from rest_framework.serializers import ModelSerializer
 
 from core.tasks.models import Task
-
-
-class FlowerTaskSerializer(Serializer):  # pylint: disable=abstract-method
-    task_id = CharField(read_only=True, source='task-id')
-    state = CharField(read_only=True)
-    result = JSONField(allow_null=True, read_only=True)
 
 
 class TaskBriefSerializer(ModelSerializer):
