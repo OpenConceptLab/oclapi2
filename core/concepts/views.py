1f23567a1611c76039a92fefe204f970191e3f69
Sny <sny.aggarwal@gmail.com>
2024-07-02 08:03:36 +0530

    OpenConceptLab/ocl_issues#1860 | expansion conflicting concepts

--- core/concepts/views.py
+++ core/concepts/views.py
@@ -383,7 +383,7 @@ class ConceptCascadeView(ConceptBaseView):
         if uri_param:
             queryset = queryset.filter(**Concept.get_parent_and_owner_filters_from_uri(uri_param))
 
-        if queryset.count() > 1:
+        if queryset.count() > 1 and 'collection' in self.kwargs:
             raise Http409()
 
         instance = queryset.first()
