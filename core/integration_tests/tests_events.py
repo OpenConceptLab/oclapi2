102e0ea6bfe40175bff6cc43d597ca511a419c0d
Sny <sny.aggarwal@gmail.com>
2024-08-30 15:51:28 +0530

    OpenConceptLab/ocl_issues#1780 | fixing pylint error

--- core/integration_tests/tests_events.py
+++ core/integration_tests/tests_events.py
@@ -5,7 +5,7 @@ from core.users.tests.factories import UserProfileFactory
 
 
 class EventsViewTest(OCLAPITestCase):
-    def test_get(self):  # pylint:disable=too-many-statements
+    def test_get(self):  # pylint:disable=too-many-statements,too-many-locals
         alfred = UserProfileFactory(username='alfred', first_name='Alfred', last_name='Pennyworth')
         bruce = UserProfileFactory(username='bruce', first_name='Bruce', last_name='Wayne')
         joker = UserProfileFactory(username='joker', first_name='Joker', last_name='The Clown')
