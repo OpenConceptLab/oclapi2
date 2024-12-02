0e76ec40769902e6aaf3629d9adb8b8921728005
Sny <sny.aggarwal@gmail.com>
2024-12-02 11:56:20 +0530

    OpenConceptLab/ocl_issues#1946 | added return path to admin or email host user

--- core/common/tasks.py
+++ core/common/tasks.py
@@ -684,7 +684,8 @@ def resources_report(start_date=None, end_date=None, email=None):  # pragma: no 
     mail = EmailMessage(
         subject=f"{env} Monthly Resources Report: {date_range_label}",
         body=f"Please find attached resources report of {env} for the period of {date_range_label}",
-        to=[reports_email]
+        to=[reports_email],
+        headers={'Return-Path': get(settings.ADMINS, '0.1') or settings.EMAIL_HOST_USER}
     )
     mail.attach(file_name, buff.getvalue(), 'text/csv')
     result = mail.send()
