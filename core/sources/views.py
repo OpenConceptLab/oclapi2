5499859edd5b3f070d36175f679fbecc7fb82d5e
Sny <sny.aggarwal@gmail.com>
2025-05-28 09:33:46 +0530

    Fixing clone operation permission

--- core/sources/views.py
+++ core/sources/views.py
@@ -363,6 +363,7 @@ class SourceMappingsIndexView(SourceBaseView):
 
 class SourceConceptsCloneView(SourceBaseView):
     serializer_class = BundleSerializer
+    permission_classes = (CanEditConceptDictionary, )
 
     def post(self, request, **kwargs):  # pylint: disable=unused-argument, too-many-locals
         """
