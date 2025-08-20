07d4b3ed3fcff7cb74cbe2797e640391a09eac72
Sny <sny.aggarwal@gmail.com>
2025-08-20 07:27:52 +0530

    OpenConceptLab/ocl_issues#2158 | updated serializer

--- core/map_projects/serializers.py
+++ core/map_projects/serializers.py
@@ -21,7 +21,8 @@ class MapProjectCreateUpdateSerializer(serializers.ModelSerializer):
             'id', 'name', 'input_file_name', 'matches', 'columns',
             'created_by', 'updated_by', 'created_at', 'updated_at', 'url', 'is_active',
             'public_access', 'file', 'user_id', 'organization_id', 'description',
-            'target_repo_url', 'matching_algorithm', 'include_retired', 'match_api_url', 'match_api_token'
+            'target_repo_url', 'matching_algorithm', 'include_retired', 'match_api_url', 'match_api_token',
+            'score_configuration'
         ]
 
     def prepare_object(self, validated_data, instance=None, file=None):
@@ -35,7 +36,7 @@ class MapProjectCreateUpdateSerializer(serializers.ModelSerializer):
             instance.columns = columns
         for attr in [
             'name', 'description', 'extras', 'target_repo_url', 'matching_algorithm', 'include_retired',
-            'match_api_url', 'match_api_token'
+            'match_api_url', 'match_api_token', 'score_configuration'
         ]:
             setattr(instance, attr, validated_data.get(attr, get(instance, attr)))
         if not instance.id:
@@ -89,7 +90,7 @@ class MapProjectSerializer(serializers.ModelSerializer):
             'created_by', 'updated_by', 'created_at', 'updated_at', 'url', 'is_active',
             'owner', 'owner_type', 'owner_url', 'public_access',
             'target_repo_url', 'matching_algorithm', 'summary', 'logs', 'include_retired',
-            'match_api_url', 'match_api_token'
+            'match_api_url', 'match_api_token', 'score_configuration'
         ]
 
     def __init__(self, *args, **kwargs):
