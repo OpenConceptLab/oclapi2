c512933150f2b325f4208eb04ff8022791315412
Sny <sny.aggarwal@gmail.com>
2025-09-11 17:21:56 +0530

    OpenConceptLab/ocl_issues#2211 | concept properties to be sorted by the latest source version it is in

--- core/concepts/models.py
+++ core/concepts/models.py
@@ -692,7 +692,7 @@ class Concept(ConceptValidationMixin, SourceChildMixin, VersionedModel):  # pyli
         return definitions
 
     def get_parent_source_version(self):
-        if self.is_versioned_object or self.is_latest_version:
+        if self.is_versioned_object:
             return self.parent
 
         return self.latest_source_version or self.get_source_version_before_creation()
