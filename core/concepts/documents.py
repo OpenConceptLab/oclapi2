3064f8b7e9abfc855ef9a1ca409cf8aaf3f024dd
Sny <sny.aggarwal@gmail.com>
2025-09-18 09:41:22 +0530

    Using filter instead of query, term instead of match to cache better in ES | using rescore to boost exact matches

--- core/concepts/documents.py
+++ core/concepts/documents.py
@@ -1,6 +1,5 @@
 from django_elasticsearch_dsl import Document, fields
 from django_elasticsearch_dsl.registries import registry
-from elasticsearch_dsl import DenseVector
 from pydash import compact, get
 
 from core.common.utils import jsonify_safe, flatten_dict, get_embeddings, drop_version
@@ -90,10 +89,13 @@ class ConceptDocument(Document):
     def get_exact_match_attrs():
         return {
             'id': {
-                'boost': 50
+                'boost': 15
             },
             'name': {
                 'boost': 15
+            },
+            'synonyms': {
+                'boost': 10
             },
             'external_id': {
                 'boost': 6
@@ -110,10 +112,10 @@ class ConceptDocument(Document):
     def get_wildcard_search_attrs():
         return {
             'id': {
-                'boost': 35
+                'boost': 5
             },
             'name': {
-                'boost': 23
+                'boost': 3
             },
             'synonyms': {
                 'boost': 0.3,
