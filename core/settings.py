f225cc3dd5c8e5eb584798787fef88531d1758a9
Sny <sny.aggarwal@gmail.com>
2025-10-28 10:33:26 +0530

    OpenConceptLab/ocl_online#8 | Removed AI Assitant API using LiteLLM from core

--- core/settings.py
+++ core/settings.py
@@ -609,5 +609,3 @@ MINIO_SECURE = os.environ.get('MINIO_SECURE') == 'TRUE'
 if ENV not in ['ci', 'demo']:
     LM_MODEL_NAME = 'all-MiniLM-L6-v2'
     LM = SentenceTransformer(LM_MODEL_NAME)
-
-ANTHROPIC_API_KEY = os.environ.get('ANTHROPIC_API_KEY', None)
