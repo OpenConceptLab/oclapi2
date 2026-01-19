7591a3f46a9eb2e9d746caf6a2d005765d7d4aee
Sny <sny.aggarwal@gmail.com>
2026-01-19 11:13:05 +0530

    GraphQL user group

--- core/graphql/views.py
+++ core/graphql/views.py
@@ -8,16 +8,18 @@ The authenticated user is passed in the GraphQL context as 'user'.
 """
 
 from asgiref.sync import sync_to_async
-from strawberry.django.views import AsyncGraphQLView
-from rest_framework.authentication import get_authorization_header
-from rest_framework.exceptions import AuthenticationFailed
-from rest_framework.request import Request
 from django.contrib.auth.models import AnonymousUser
 from django.middleware.csrf import CsrfViewMiddleware, get_token
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework.authentication import get_authorization_header
+from rest_framework.exceptions import AuthenticationFailed
+from rest_framework.request import Request
+from strawberry.django.views import AsyncGraphQLView
+from strawberry.exceptions import GraphQLError
 
 from core.common.authentication import OCLAuthentication
+from core.users.constants import GRAPHQL_API_GROUP
 
 
 # https://strawberry.rocks/docs/breaking-changes/0.243.0 GraphQL Strawberry needs manually handling CSRF
@@ -62,9 +64,14 @@ class AuthenticatedGraphQLView(AsyncGraphQLView):
 
         # First, check if user is authenticated via session (e.g., browser login)
         if hasattr(request, 'user') and request.user.is_authenticated:
-            context.user = request.user
-            context.auth_status = 'valid'
-            return context
+            if await sync_to_async(request.user.has_auth_group, thread_sensitive=True)(GRAPHQL_API_GROUP):
+                context.user = request.user
+                context.auth_status = 'valid'
+                return context
+            else:
+                context.user = AnonymousUser()
+                context.auth_status = 'invalid'
+                return context
 
         # Otherwise, check authorization header
         auth_header = get_authorization_header(request)
@@ -90,6 +97,11 @@ class AuthenticatedGraphQLView(AsyncGraphQLView):
             return context
 
         user, auth = auth_result
+        if not (await sync_to_async(user.has_auth_group, thread_sensitive=True)(GRAPHQL_API_GROUP)):
+            context.user = AnonymousUser()
+            context.auth_status = 'invalid'
+            return context
+
         context.user = user
         context.auth = auth
         context.auth_status = 'valid' if getattr(user, 'is_authenticated', False) else 'invalid'
