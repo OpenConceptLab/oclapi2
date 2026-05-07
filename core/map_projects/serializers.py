f4e50b3ad28bf953b013c91d3b84538c0af82337
Sunny Aggarwal <sny.aggarwal@gmail.com>
2026-05-07 17:55:35 +0530

    OpenConceptLab/ocl_issues#2383 | renamed to prompt_template_key

--- core/map_projects/serializers.py
+++ core/map_projects/serializers.py
@@ -23,7 +23,7 @@ class MapProjectCreateUpdateSerializer(serializers.ModelSerializer):
             'public_access', 'file', 'user_id', 'organization_id', 'description',
             'target_repo_url', 'include_retired', 'score_configuration',
             'filters', 'candidates', 'algorithms', 'lookup_config', 'analysis', 'encoder_model',
-            'prompt_template_url'
+            'prompt_template_key'
         ]
 
     def prepare_object(self, validated_data, instance=None, file=None):
@@ -38,7 +38,7 @@ class MapProjectCreateUpdateSerializer(serializers.ModelSerializer):
         for attr in [
             'name', 'description', 'extras', 'target_repo_url', 'include_retired',
             'score_configuration', 'filters', 'candidates', 'algorithms', 'lookup_config', 'analysis',
-            'encoder_model', 'prompt_template_url'
+            'encoder_model', 'prompt_template_key'
         ]:
             setattr(instance, attr, validated_data.get(attr, get(instance, attr)))
         if not instance.id:
