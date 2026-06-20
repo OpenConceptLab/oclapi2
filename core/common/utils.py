fe1f256cf2261511fbcdb81f9b0e3fa4b6f7a802
Sunny Aggarwal <sny.aggarwal@gmail.com>
2026-06-20 06:56:56 +0530

    OpenConceptLab/ocl_issues#2581 | eager loading more

--- core/common/utils.py
+++ core/common/utils.py
@@ -336,7 +336,9 @@ def write_export_file(
                 'parent', 'parent__organization', 'parent__user',
                 'from_source', 'from_source__organization', 'from_source__user',
                 'to_source', 'to_source__organization', 'to_source__user',
-                'from_concept', 'to_concept', 'created_by', 'updated_by',
+                'from_concept', 'from_concept__parent',
+                'to_concept', 'to_concept__parent',
+                'created_by', 'updated_by',
             ).prefetch_related('from_concept__names', 'to_concept__names').order_by('-id')
             mapping_versions = list(queryset)
             if mapping_versions:
