7cea946b29a0c595b5361d4620a5da9867d481d7
Sunny Aggarwal <sny.aggarwal@gmail.com>
2026-08-10 12:08:10 +0530

    OpenConceptLab/ocl_issues#2598 | partial indexing is needed for public_access change and nothing on is_active

--- core/sources/signals.py
+++ core/sources/signals.py
@@ -10,22 +10,20 @@ def propagate_parent_attributes(sender, instance=None, created=False, **kwargs):
     if created:
         instance.record_create_event()
     if not created and instance:
-        updated_concepts = 0
-        updated_mappings = 0
-        if get(instance, '_should_update_public_access'):
-            updated_concepts += instance.concepts_set.exclude(
-                public_access=instance.public_access).update(public_access=instance.public_access)
-            updated_mappings += instance.mappings_set.exclude(
-                public_access=instance.public_access).update(public_access=instance.public_access)
         if get(instance, '_should_update_is_active'):
-            updated_concepts += instance.concepts_set.exclude(
-                is_active=instance.is_active).update(is_active=instance.is_active)
-            updated_mappings += instance.mappings_set.exclude(
-                is_active=instance.is_active).update(is_active=instance.is_active)
+            instance.concepts_set.exclude(is_active=instance.is_active).update(is_active=instance.is_active)
+            instance.mappings_set.exclude(is_active=instance.is_active).update(is_active=instance.is_active)
 
-        if updated_concepts:
-            from core.concepts.documents import ConceptDocument
-            instance.batch_index(instance.concepts_set, ConceptDocument)
-        if updated_mappings:
-            from core.mappings.documents import MappingDocument
-            instance.batch_index(instance.mappings_set, MappingDocument)
+        if get(instance, '_should_update_public_access'):
+            updated_concepts = instance.concepts_set.exclude(
+                public_access=instance.public_access).update(public_access=instance.public_access)
+            updated_mappings = instance.mappings_set.exclude(
+                public_access=instance.public_access).update(public_access=instance.public_access)
+
+            partial_doc = {'public_can_view': instance.public_can_view}
+            if updated_concepts:
+                from core.concepts.documents import ConceptDocument
+                instance.batch_index(instance.concepts_set, ConceptDocument, partial_doc=partial_doc)
+            if updated_mappings:
+                from core.mappings.documents import MappingDocument
+                instance.batch_index(instance.mappings_set, MappingDocument, partial_doc=partial_doc)
